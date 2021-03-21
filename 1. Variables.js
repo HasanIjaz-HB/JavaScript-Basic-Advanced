74faf7e00c3a6b5db0cb1c1d90af939ca9a97b89
Hasan Ijaz <hasanijaz@sabanciuniv.edu>
2021-03-22 03:17:01 +0500

    First

---
+++ 1. Variables.js
@@ -0,0 +1,22 @@
+//You can think of variables as a way to store some data. Think of them as containers which store values for you.
+
+
+console.clear(); //to clear console
+
+var num1 = 2 // var = variable declaration, num1 is variable name, = store , 2 is value to be store in num1
+var num2 = 4
+var num3 = 6
+
+var sum = num1 + num2 + num3 // + operator to add, = operator to assign
+
+//expression is a line of code which contains a valid combination of variables, values and operators all above lines are expressions
+
+//to print something on screen
+
+console.log(sum)
+
+//we can store any of the data types in variables
+
+var firstName = "Qaifi"
+
+console.log(firstName)
